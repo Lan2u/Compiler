74daf5ae8548973cfb8a26a9005522d52931bae9
Eva <evalott100@gmail.com>
2017-12-29 13:11:40 +0000

    Fixed a little indentation

--- src/RPN/postfix.hpp
+++ src/RPN/postfix.hpp
@@ -2,8 +2,8 @@
 #define POSTFIX_HPP
 // postfix.hpp
 // Paul Lancaster and Eva Lott
-// Created29/12/17
-// Edited29/12/17
+// Created	29/12/17
+// Edited	29/12/17
 // Responisble for conversion of string infix 
 // sequences into postfix and solving postfix (rpn)
 // expressions. The precendence of operators is as follows...
@@ -11,29 +11,28 @@
 
 #include "list.hpp"
 
-#include <iostream>
 #include <string>
 
 // Constants for order of operations
 // Staic class Postfix
 class Postfix {
 private:
-// Constant struct to remember precidence
-const struct {
-	unsigned short DIVISION = 4;
-	unsigned short MULTIPLICATION = 4;
-	unsigned short REMAINDER = 4;
-	unsigned short ADDITION = 3;
-	unsigned short SUBTRACTION = 3;
-	unsigned short POWER = 5;
-	unsigned short EQUALS = 0;
-	unsigned short NEGATION = 1;
-	unsigned short AND = 1;
-	unsigned short OR = 1;
-	unsigned short COMPARISON = 2;
-} precidence;
+	// Constant struct to remember precidence
+	const struct {
+		unsigned short DIVISION = 4;
+		unsigned short MULTIPLICATION = 4;
+		unsigned short REMAINDER = 4;
+		unsigned short ADDITION = 3;
+		unsigned short SUBTRACTION = 3;
+		unsigned short POWER = 5;
+		unsigned short EQUALS = 0;
+		unsigned short NEGATION = 1;
+		unsigned short AND = 1;
+		unsigned short OR = 1;
+		unsigned short COMPARISON = 2;
+	} precidence;
 
-bool isNumber (char);
+	bool isNumber (char);
 
 public:
 	static unsigned getOperatorPrecidence (std::string);
