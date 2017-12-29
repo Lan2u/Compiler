d2383b61c4932349bc9317731e33563531698878
Eva <evalott100@gmail.com>
2017-12-29 00:08:39 +0000

    Renamed files to lowercase beginnings... Sorry it's standard. Working on OO based list class using templates and polymorphism. Cleared the ordinary C files to 'Previous_c_files' as they will need to be rewritten in C++ anyways. Added preproc guards to the headers already written, and provided a function declaration for the given .cpp file in it. Changed an include as it was a .cpp file and we include nothing but headers or compiled bytecode libraries.

--- src/RPN/list.hpp
+++ src/RPN/list.hpp
@@ -2,27 +2,27 @@
 #define LIST_HPP
 // list.hpp
 // Eva Lott
+// Created	28/12/17
+// Edited	28/12/17
 // Contains an abstract linked list class and derived
 // classes for stack and queue data structures.
-
-#include <iostream>
 
 // Abstract List class
 template <class Temp>
 class List {
-protected:
+public:
 	List (void);
 	~List (void);
+	virtual void push (Temp data);
 	virtual long unsigned getLength (void);
 
-private:
+protected:
+	long unsigned length;
 	struct Node {
 		Temp data;
 		long unsigned pos;
 		Node *next, *previous;
 	} *head, *tail;
-
-	long unsigned length;
 };
 
 // Constructor, initialise fields
@@ -37,11 +37,26 @@ List<Temp>::List (void)
 template <class Temp> 
 List<Temp>::~List (void)
 {
-	Temp *n = head, *tn;
+	Node *n = head, *tn;
 	while ((tn = n) != nullptr) {
 		n = n->next;
 		delete tn;
 	}
+}
+
+// Push the tail
+template <class Temp> 
+void List<Temp>::push (Temp data)
+{
+	// Create a new node and set it's fields
+	Node *newNode = new Node;
+	newNode->data = data;
+	newNode->pos = ++length;
+	newNode->previous = tail;
+	newNode->next = nullptr;
+
+	// Set the tail to newNode
+	tail = newNode;
 }
 
 // Getters
@@ -53,16 +68,60 @@ long unsigned List<Temp>::getLength (void) {return length;}
 template <class Temp>
 class Stack : public List<Temp> {
 public:
-	bool push (Temp);
 	Temp pop (void);
 };
 
-// Push the tail
-template <class Temp> 
-bool Stack<Temp>::push (Temp)
+// Pop the value of the tail then destroy it
+template <class Temp>
+Temp Stack<Temp>::pop (void)
 {
-	Node *n = List<Temp>::tail, *tn;
+	// Save the value of temp for return later
+	Temp t = List<Temp>::tail->data;
+
+	// Save the memory location of the old tail
+	List<Temp>::Node n = List<Temp>::tail;
+	
+	// Set the tail to the previous node and it's next to null
+	List<Temp>::tail = List<Temp>::tail->previous;
+	List<Temp>::tail->next = nullptr;
+
+	// Delete the old tail
+	delete tn;
+
+	--List<Temp>::length;
+
+	// Return the old data value
+	return t;
 }
 
+// Queue derived class
+template <class Temp>
+class Queue : public List<Temp> {
+public:
+	Temp pop (void);
+};
+
+// Pop the value of the head then destroy it
+template <class Temp>
+Temp Queue<Temp>::pop (void)
+{
+	// Save the value of temp for return later
+	Temp t = List<Temp>::head->data;
+
+	// Save the memory location of the old head
+	List<Temp>::Node *tn = List<Temp>::head;
+	
+	// Set the head to the next node and it's previos to null
+	List<Temp>::head = List<Temp>::head->next;
+	List<Temp>::head->previous = nullptr;
+
+	// Delete the old tail
+	delete tn;
+
+	--List<Temp>::length;
+
+	// Return the old data value
+	return t;
+}
 
 #endif // LIST_HPP
