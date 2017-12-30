2b87dac97dd90f901b7ca5bf9a75d219b55266ff
Paul Lancaster <paul@lancasterzone.com>
2017-12-30 18:17:47 +0000

    Started adding the ability to specify the accepting states token type for the tokenising FSA

--- src/Tokeniser/tokeniser.cpp
+++ src/Tokeniser/tokeniser.cpp
@@ -2,8 +2,17 @@
 
 #define FSA_DEF_DELIMITER ","
 
-State* Tokeniser::findState(std::string id, std::list<state> ) {
-	
+// Find the state with the given id in the list of states and return a pointer to it or a nullptr if not found
+State* Tokeniser::findState(std::string id, List<State*>* states) {
+	// TODO implement
+	return nullptr;
+}
+
+// Get the token that is represented by the given token type string. Eg. OPERATOR would give a token of OPERATOR type. 
+// Initially this is statically hard coded however it should probably be stored in a file
+Token* Tokeniser::getAcceptingTokenType(std::string tokenTypeStr) {
+	// TODO
+	return nullptr;
 }
 
 Tokeniser::Tokeniser(std::string fsaDefinitionFilePath){	
@@ -22,14 +31,14 @@ Tokeniser::Tokeniser(std::string fsaDefinitionFilePath){
 		std::string initialStateId;
 		std::string input;
 		std::string finalStateId;
-		std::string accepting;
+		std::string acceptingTokenTypeStr;
 		
 		strStream >> initialStateId;
 		strStream >> input;
 		strStream >> finalStateId;
 		
 		// TODO handle there not being 4 pieces of the definition
-		strStream >> accepting;
+		strStream >> acceptingTokenTypeStr;
 
 		State* initialState = findState(initialStateId);
 		if (initialState == nullptr) {
@@ -43,34 +52,57 @@ Tokeniser::Tokeniser(std::string fsaDefinitionFilePath){
 			states.push(finalState);
 		}
 
-		if (accepting == ACCEPTING_TOKEN) {
-			finalState->setAccepting(true);
+		
+		if (acceptingTokenTypeStr != "") {
+			Token* tokenType = getAcceptingTokenType(acceptingTokenTypeStr);
+			finalState->setAccepting(tokenType);
 		}
+		
+		
 
 		initialState->addTransition(input, finalState);
 	}
 
 }
 
-Queue<Token>* Tokeniser::tokeniseString(std::string)
+Queue<Token>* Tokeniser::tokeniseString(std::string str)
 {
-	
+	reset();
+	for (int i = 0; i < str.length; i++) {
+		currentState = currentState->getNext(str.at[i]);
+	}
+
+
+}
+
+void Tokeniser::reset()
+{
+	currentState = initialState;
 }
 
 State::State(std::string id)
 {
-
+	stateId = id; 
+	acceptingTokenType = nullptr;
 }
 
-void State::setAccepting(bool accepting) {
-	acceptingState = accepting;
+void State::setAccepting(Token* type) {
+	acceptingTokenType = type;
 }
 
-void State::addTransition(std::string, State *)
+void State::addTransition(std::string input, State * finalState)
 {
+	transitions.insert({ input, finalState });
 }
 
-State * State::getNext(std::string)
+State * State::getNext(std::string input)
 {
-	return nullptr;
+	State* next = transitions[input];
+	if (next == nullptr) {
+		// No registered/defined input->final transition indicates that the input has no effect and the state doesn't change.
+		return this;
+	}
+	else {
+		return next;
+	}
 }
--- src/Tokeniser/tokeniser.hpp
+++ src/Tokeniser/tokeniser.hpp
@@ -13,28 +13,33 @@
 #include <iostream>
 #include <sstream>
 
-/*
-An nFSA based tokeniser. 
-*/
-class Tokeniser {
-public:
-	State * findState(std::string id);
-	Tokeniser(std::string fsaDefinitionFilePath);
-	Queue<Token>* tokeniseString(std::string);
-};
-
 // Representation of a state within the nFSA. 
 class State {
 private:
 	// Represents the state transition function of the FSA for this state
 	std::unordered_map<std::string, State*> transitions;
-	std::string id;
-	bool acceptingState;
+	std::string stateId;
+	Token* acceptingTokenType;
 public: 
 	State(std::string id);
 	void addTransition(std::string, State*);
 	State* getNext(std::string);
-	void setAccepting(bool accepting);
+	void setAccepting(Token*);
 };
 
+/*
+An nFSA based tokeniser.
+*
+*/
+class Tokeniser {
+private:
+	State * initialState;
+	State * currentState;
+	State * Tokeniser::findState(std::string, List<State*>*);
+	Token * getAcceptingTokenType(std::string);
+public:
+	Tokeniser(std::string fsaDefinitionFilePath);
+	Queue<Token>* tokeniseString(std::string);
+	void reset();
+};
 #endif // TOKENISER_HPP
