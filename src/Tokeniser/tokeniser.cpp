0b767f2125be9f46def14e1cf9fda177fa4b0924
Paul Lancaster <paul@lancasterzone.com>
2017-12-30 17:53:45 +0000

    Added creation of tokenising FSA from definitions file

--- src/Tokeniser/tokeniser.cpp
+++ src/Tokeniser/tokeniser.cpp
@@ -1,17 +1,53 @@
 #include "tokeniser.hpp"
 
+#define FSA_DEF_DELIMITER ","
 
-Tokeniser::Tokeniser(std::string fsaDefinitionFilePath){
-	std::ifstream* fsaFileStream = new std::ifstream(fsaDefinitionFilePath);
-	if (!fsaFileStream->good()) {
+State* Tokeniser::findState(std::string id, std::list<state> ) {
+	
+}
+
+Tokeniser::Tokeniser(std::string fsaDefinitionFilePath){	
+	std::ifstream fsaFileStream(fsaDefinitionFilePath);
+	if (!fsaFileStream.good()) {
 		// TODO File stream error
+		return;
 	}
 
+	List<State*> states; // TODO Replacing with a map will be more efficient however it is not a priority.
 
+	for (std::string definitionLine; std::getline(fsaFileStream, definitionLine); ) {
+		// Parse the state
+		std::stringstream strStream(definitionLine);
+		
+		std::string initialStateId;
+		std::string input;
+		std::string finalStateId;
+		std::string accepting;
+		
+		strStream >> initialStateId;
+		strStream >> input;
+		strStream >> finalStateId;
+		
+		// TODO handle there not being 4 pieces of the definition
+		strStream >> accepting;
 
-	std::string definitionLine;
-	while (std::getline(fsaFileStream, definitionLine)) {
+		State* initialState = findState(initialStateId);
+		if (initialState == nullptr) {
+			initialState = new State(initialStateId);
+			states.push(initialState);
+		}
 
+		State* finalState = findState(finalStateId);
+		if (finalState == nullptr) {
+			finalState = new State(finalStateId);
+			states.push(finalState);
+		}
+
+		if (accepting == ACCEPTING_TOKEN) {
+			finalState->setAccepting(true);
+		}
+
+		initialState->addTransition(input, finalState);
 	}
 
 }
@@ -21,9 +57,13 @@ Queue<Token>* Tokeniser::tokeniseString(std::string)
 	
 }
 
-State::State(int id, bool accepting)
+State::State(std::string id)
 {
 
+}
+
+void State::setAccepting(bool accepting) {
+	acceptingState = accepting;
 }
 
 void State::addTransition(std::string, State *)
--- src/Tokeniser/tokeniser.hpp
+++ src/Tokeniser/tokeniser.hpp
@@ -1,6 +1,8 @@
 #ifndef TOKENISER_HPP
 #define TOKENISER_HPP
 
+#define ACCEPTING_TOKEN "*"
+
 // Paul Lancaster
 
 #include "token.hpp"
@@ -9,12 +11,14 @@
 #include "../RPN/list.hpp"
 #include <fstream>
 #include <iostream>
+#include <sstream>
 
 /*
 An nFSA based tokeniser. 
 */
 class Tokeniser {
 public:
+	State * findState(std::string id);
 	Tokeniser(std::string fsaDefinitionFilePath);
 	Queue<Token>* tokeniseString(std::string);
 };
@@ -24,10 +28,13 @@ class State {
 private:
 	// Represents the state transition function of the FSA for this state
 	std::unordered_map<std::string, State*> transitions;
+	std::string id;
+	bool acceptingState;
 public: 
-	State(int id, bool accepting);
+	State(std::string id);
 	void addTransition(std::string, State*);
 	State* getNext(std::string);
+	void setAccepting(bool accepting);
 };
 
 #endif // TOKENISER_HPP
