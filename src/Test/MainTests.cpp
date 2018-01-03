96d77d08406323e315742e944d368dd1081af069
Paul <paul@lancasterzone.com>
2018-01-03 23:08:03 +0000

    Added some queue tests. Fails.

--- src/Test/MainTests.cpp
+++ src/Test/MainTests.cpp
@@ -17,4 +17,69 @@ BOOST_AUTO_TEST_CASE(Queue_Create_Test) {
 	BOOST_TEST(q.getLength() == 0);
 }
 
+BOOST_AUTO_TEST_CASE(Queue_Add_One_Test) {
+	Queue<int> q;
+	int a = 2;
+	q.push(a);
+	BOOST_TEST(q.getLength() == 1);
+}
+
+BOOST_AUTO_TEST_CASE(Queue_Add_One_Remove_One_Test) {
+	Queue<int> q;
+	int a = 2;
+	q.push(a);
+	int b = q.pop();
+	BOOST_TEST(a == b);
+	BOOST_TEST(q.getLength() == 0);
+}
+
+BOOST_AUTO_TEST_CASE(Queue_Add_Three_Remove_All_Test) {
+	Queue<int> q;
+	int a = 2;
+	int b = 3;
+	int c = 4;
+	q.push(a);
+	q.push(b);
+	q.push(c);
+
+	BOOST_TEST(q.getLength() == 3);
+
+	BOOST_TEST(a == q.pop());
+	BOOST_TEST(b == q.pop());
+	BOOST_TEST(c == q.pop());
+
+	BOOST_TEST(q.getLength() == 0);
+}
+
+BOOST_AUTO_TEST_CASE(Queue_Append_Test) {
+	Queue<int> queue1;
+	Queue<int> queue2;
+
+	int a = 1;
+	int b = 2;
+	int c = 3;
+	int d = 4;
+	int e = 5;
+	int f = 6;
+
+	queue1.push(a);
+	queue1.push(b);
+	queue1.push(c);
+
+	queue2.push(d);
+	queue2.push(e);
+	queue2.push(f);
+
+	queue1.append(&queue2);
+
+	BOOST_TEST(queue1.getLength() == 6);
+
+	BOOST_TEST(a == queue1.pop());
+	BOOST_TEST(b == queue1.pop());
+	BOOST_TEST(c == queue1.pop());
+	BOOST_TEST(d == queue1.pop());
+	BOOST_TEST(e == queue1.pop());
+	BOOST_TEST(f == queue1.pop());
+}
+
 BOOST_AUTO_TEST_SUITE_END()
