5d01efe5f926a586dd6678aa8529c3d574acccff
Eva <evalott100@gmail.com>
2018-01-05 15:28:05 +0000

    Changed data structures to favour the current inheritance setup

---
+++ src/DataStructures/tree.hpp
@@ -0,0 +1,63 @@
+#ifndef TREE_HPP
+#define TREE_HPP
+
+#include "queue.hpp"
+#include "../Tokeniser/token.hpp"
+
+template <class T>
+class BinaryTree {
+private:
+	class Node {
+	public:
+		Node *left, *right;
+
+		Node (T);
+		Queue<T*>* getPostfixRepresentation (void);
+
+		// Getters
+		T* getElement(void) const {return element;};
+
+	private:
+		T *element;
+
+	} root;
+
+public:
+	BinaryTree (void);
+	~BinaryTree (void);
+
+	Queue<T*>* getPostfixRepresentation (void);
+	bool add (T*);
+	bool remove (T*);
+};
+
+template <class T>
+Queue<T*>* BinaryTree<T>::Node::getPostfixRepresentation (void)
+{
+	Queue<Token*> *queue = new Queue<Token*>;
+
+	if (left != nullptr) {
+		queue->enqueue(left->getPostfixRepresentation());
+	}
+	if (right != nullptr) {
+		queue->enqueue(right->getPostfixRepresentation());
+	}
+
+	queue->enqueue(this.getElement());
+	return queue;
+}
+
+template <class T>
+Queue<T*>* BinaryTree<T>::getPostfixRepresentation (void)
+{
+	return root->getPostfixRepresentation();
+}
+
+// Returns true if successful and false if not
+template <class T>
+bool add (T *element)
+{
+	
+}
+
+#endif // TREE_HPP
